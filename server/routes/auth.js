05c8c7057453baa22c22905884b2229de4469f31
satvik-vm <satvik.verma@iiitb.ac.in>
2023-12-08 17:30:45 +0530

    Added client docker file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/auth.js";
+import { register, login } from "../controllers/auth.js";
 
 const router = express.Router();
 
+router.post("/register", register);
 router.post("/login", login);
 
 export default router;
